4003778083c53d4f0c3aea96ed81e8e7283013f7
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-08-31 17:38:59 -0700

    figuring out how delete works

--- express_server.js
+++ express_server.js
@@ -14,11 +14,6 @@ const urlDatabase = {
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-app.get("/", (req, res) => {
-  let templateVars = { greeting: "Hello World!"};
-  res.render("hello_world", templateVars);
-});
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -30,6 +25,13 @@ app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   let templateVars = { shortURL: req.params.shortURL, longURL:  urlDatabase[shortURL]};
   res.render('urls_show', templateVars);
+});
+app.get("/urls/:shortURL/delete", (req, res) => {
+  const urltoDelete = req.params.shortURL;
+  console.log(urltoDelete);
+  delete urlDatabase[urltoDelete];
+  console.log(urlDatabase);
+  res.redirect("urls/");
 });
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
