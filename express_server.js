5e7970e0bd24b745604e501cec2c7c18932b96c2
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-09-01 15:47:16 -0700

    added logout feature

--- express_server.js
+++ express_server.js
@@ -26,17 +26,35 @@ const urlDatabase = {
 
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
+//LOGIN
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect("/urls");
+});
+//LOGOUT
+app.post("/logout", (req, res) => {
+  res.cookie('username', req.body.username);
+  res.clearCookie('username');
+  res.redirect("/urls");
+});
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  let templateVars = { shortURL: req.params.shortURL, longURL:  urlDatabase[shortURL]};
+  let templateVars = {
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL, longURL: urlDatabase[shortURL]
+  };
   res.render('urls_show', templateVars);
 });
 
